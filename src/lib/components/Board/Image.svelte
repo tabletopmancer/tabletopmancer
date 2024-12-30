493873b32b99b0e38dd13644d9006b1e1c6fe8eb
ubermanu <manu@paca.happy-dev.fr>
2024-12-30 12:10:53 +0100

    feat: image width and height

--- src/lib/components/Board/Image.svelte
+++ src/lib/components/Board/Image.svelte
@@ -3,9 +3,17 @@
 
   let {
     src,
+    width,
+    height,
     position = [0, 0],
     scale = 1,
-  }: { src: string; position: number[]; scale: number } = $props()
+  }: {
+    src: string
+    width?: number
+    height?: number
+    position: number[]
+    scale: number
+  } = $props()
 
   // TODO: Add position + scale + rotation
 
@@ -13,6 +21,14 @@
   // TODO: What happen when the props changes?
   const image = new Image()
   image.src = src
+
+  if (width) {
+    image.width = width
+  }
+
+  if (height) {
+    image.height = height
+  }
 
   onRender(({ context, next }) => {
     if (image.complete) {
