7dcec4b6df8daeec713de87503242d68790c33d4
ubermanu <emmanuel.vodor@proton.me>
2025-03-15 10:44:13 +0100

    fix: tailwind scrollbars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,5 @@
-import scrollbar from 'tailwind-scrollbar'
 import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [scrollbar],
 } satisfies Config
